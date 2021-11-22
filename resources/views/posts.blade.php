12d9b1c2ddfbcf2811c251e7281b670e7435e463
VzMaster31 <ah067622@gmail.com>
2021-11-22 09:58:12 +0700

    menambahkan searching dan pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,26 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $title }}</h1>
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts" method="GET">
+                @if (request('category'))
+                <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+
+                @if (request('author'))
+                <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search...." name="search" value="{{ request('search') }}">
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
 
     @if ($posts->count())
@@ -16,7 +35,7 @@
 
                 <p>
                     <small class="text-muted">
-                    by. <a href="/author/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                    by. <a href="/posts?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
                     </small>
                 </p>
 
@@ -25,9 +44,7 @@
 
             </div>
         </div>
-    @else
-        <p class="text-center fs-4">No Post found.</p>
-    @endif
+    
     
     <div class="container">
         <div class="row">
@@ -36,7 +53,7 @@
             <div class="col-md-4 mb-3">
                 <div class="card">
                     <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)">
-                        <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
+                        <a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">
                             {{ $post->category->name }}
                         </a>
                     </div>
@@ -45,7 +62,7 @@
                       <h5 class="card-title">{{ $post->title }}</h5>
                       <p>
                         <small class="text-muted">
-                        by. <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                        by. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                         </small>
                       </p>
                       <p class="card-text">{{ $post->excerpt }}</p>
@@ -57,6 +74,13 @@
         </div>
     </div>
 
+    @else
+        <p class="text-center fs-4">No Post found.</p>
+    @endif
+
+    <div class="d-flex justify-content-center">
+        {{ $posts->links() }}
+    </div>
     
 @endsection
 
